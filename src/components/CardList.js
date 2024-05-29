a51134f0cef0c175771e69dac31a96edff168528
Dipesh416 <dipeshdeepgothi416@gmail.com>
2024-05-29 17:26:05 +0530

    second commit

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import './CardList.css';
+import React from "react";
+import "./CardList.css";
 
 const CardList = ({ cards }) => {
-    return (
-        <div className="card-list">
-            {cards.map((card, index) => (
-                <div key={index} className="card">
-                    <img src={card} alt='ok' width={200} height={200}/>
-                </div>
-            ))}
+  return (
+    <div className="card-list">
+      {cards.map((card, index) => (
+        <div key={index} className="card">
+          <img src={card} alt="ok" width={200} height={250} />
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default CardList;
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,15 +1,47 @@
-import './Popup.css';
-import CloseIcon from '@mui/icons-material/Close';
-const Popup=({setPopup})=>{
-    return <div className='container'>
-        <div className='box'>
-            <div className='card'>ReEagage</div>
-            <div className='card'>Able</div>
-            <div className='card'>Shop</div>
-            <div className='card'>ReCommed</div>
-            
+import "./Popup.css";
+import CloseIcon from "@mui/icons-material/Close";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import SellOutlinedIcon from "@mui/icons-material/SellOutlined";
+import InsertLinkOutlinedIcon from "@mui/icons-material/InsertLinkOutlined";
+
+const Popup = ({ setPopup }) => {
+  return (
+    <div className="container">
+      <div className="box">
+        <div id="card1" className="card">
+          <h2> ReEagage</h2>
+          <p>Get brand deals</p>
+          <div className="card-icon">
+            <SellOutlinedIcon style={{ fontSize: "3rem" }} />
+          </div>
         </div>
-        <div className='icon' onClick={()=>setPopup(false)}><CloseIcon/></div>
+        <div id="card2" className="card">
+          <h2>Able </h2>
+          <p>Sell products bellow youtube videos</p>
+          <div className="card-icon">
+            <YouTubeIcon style={{ fontSize: "3rem" }} />
+          </div>
+        </div>
+        <div id="card3" className="card">
+          <h2>Shop</h2>
+          <p>Turn content into earnings</p>
+          <div className="card-icon">
+            <ShoppingCartIcon style={{ fontSize: "3rem" }} />
+          </div>
+        </div>
+        <div id="card4" className="card">
+          <h2>ReCommed</h2>
+          <p>Sell branded products</p>
+          <div className="card-icon">
+            <InsertLinkOutlinedIcon style={{ fontSize: "3rem" }} />
+          </div>
+        </div>
+      </div>
+      <div className="icon" onClick={() => setPopup(false)}>
+        <CloseIcon />
+      </div>
     </div>
-}
+  );
+};
 export default Popup;
